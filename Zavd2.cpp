93c293501bce8e26a254daea8eff258034a413d6
Nastya-19 <nastysha_19@mail.ru>
2016-05-18 11:28:28 +0300

    Лінійне рівняння

--- Zavd2.cpp
+++ Zavd2.cpp
@@ -1,17 +1,17 @@
-#include <iostream>
-using namespace std;
+#include <stdio.h>
+#include <stdlib.h>
 
-int main () 
-{
-	setlocale(0,"");
-	int a, b ;
-	cout << "ax + b = 0\n" ;
-	cout << "¬ведите а :" ;
-	cin >> a ;
-	cout << "¬ведите b :\n" ;
-	cin >> b ;
-	int x = -b/a ;
-	cout << "x = " << x << endl ;
-
+int main(int argc, char *argv[]) {
+	int a, b, x ;
+	
+	printf("ax + b = 0\n") ;
+	printf(Vvedite a:") ;
+	scanf("%d", &a) ;
+	printf("Vvedite b:");
+	scanf("%d", &d) ;
+	x = -b/a ;
+	printf("x = %d", x);
+	
+	system("pause");
 	return 0;
 }
